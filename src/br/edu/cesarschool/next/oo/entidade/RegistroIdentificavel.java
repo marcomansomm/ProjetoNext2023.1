cbeb99096c3ff38afc04e7a47b0b3d00dfbfe5fc
marcomansomm <marco.santos@tascomtecnologia.com.br>
2023-08-03 21:35:39 -0300

    denovo

---
+++ src/br/edu/cesarschool/next/oo/entidade/RegistroIdentificavel.java
@@ -0,0 +1,13 @@
+package br.edu.cesarschool.next.oo.entidade;
+
+
+/*
+ * Classe RegistroIdentificavel
+    Criar tal classe abstrata, que representa dados gerais que toda entidade possa ter, e a
+    característica de que toda entidade é identificável por uma chave única, que será definida
+    em cada uma das subsclasses desta classe, através da implementação obrigatória de um
+    método abstrato.
+ */
+public abstract class RegistroIdentificavel {
+    
+}
